e1db6d00ebda473a6e90ba9644e99dd6b4f4a4d7
andro98 <andrewaacodo12@gmail.com>
2019-07-04 15:50:29 +0200

    Working with Lists & Conditionals

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+//import 'package:flutter/rendering.dart';
 
 import './product_manager.dart';
 
 //This notation is valid only if the method contain one line only
-void main() => runApp(MyApp());
+void main() {
+  //debugPaintSizeEnabled = true;
+  //debugPaintBaselinesEnabled = true;
+  //debugPaintPointersEnabled = true;
+  runApp(MyApp());
+}
 
+//final => doesn't not change the reference in memory, cannot assign new value, but can add
+//const =>
 //stateless widget for static pages
 //stateful widget for dynamic pages
 //scaffold create new page in app
@@ -13,17 +21,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+    //debugShowMaterialGrid: true,
       theme: ThemeData(
-        brightness: Brightness.light,
-        primarySwatch: Colors.deepOrange,
-        accentColor: Colors.deepPurple
-      ),
+          brightness: Brightness.light,
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.deepPurple),
       home: Scaffold(
           appBar: AppBar(
             title: Text('Menu El 3azim'),
             elevation: 10.0,
           ),
-          body: ProductManager('Food Tester')),
+          body: ProductManager()),
     );
   }
 }
--- lib/products.dart
+++ lib/products.dart
@@ -1,25 +1,41 @@
 import 'package:flutter/material.dart';
 
 class Products extends StatelessWidget {
-  final List<String>products;
-  
-  Products(this.products); 
+  final List<String> products;
+
+  Products(this.products);
+
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      elevation: 20.0,
+      margin: EdgeInsets.all(10),
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text(products[index]),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProductLists() {
+    Widget productCard;
+
+    if (products.length > 0) {
+      productCard = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    } else {
+      productCard = Center(
+        child: Text('No Product found, PLease add some'),
+      );
+    }
+    return productCard;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: products
-          .map((element) => Card(
-                elevation: 20.0,
-                margin: EdgeInsets.all(10),
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/food.jpg'),
-                    Text(element),
-                  ],
-                ),
-              ))
-          .toList(),
-    );
+    return _buildProductLists();
   }
 }
